fc448371fbd12b9fe231493a34dfdb1d4fcf59cf
zippolaur <74960474+zippolaur@users.noreply.github.com>
2026-03-16 16:11:37 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-"from fastapi import FastAPI, APIRouter, HTTPException, Request, Depends, Header
+from fastapi import FastAPI, APIRouter, HTTPException, Request, Depends, Header
 from fastapi.security import HTTPBearer
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
